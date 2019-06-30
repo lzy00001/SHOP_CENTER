44b048900864edb283ff122ffac3c70b53d9b357
lzy00002 <812459596@qq.com>
2019-06-30 21:57:11 +0800

    测试

--- shopping_mall/shopping_mall/apps/users/urls.py
+++ shopping_mall/shopping_mall/apps/users/urls.py
@@ -7,3 +7,4 @@ urlpatterns = [
     url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
     url(r'^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
 ]
+
