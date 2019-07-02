72b86cf2e32816f7da91f813a63ee2fd584cb0d4
lzy00002 <812459596@qq.com>
2019-07-02 16:06:20 +0800

    修改了域名配置，因为要登录QQ验证必须使用meiduo,qq登录第一步完成

---
+++ shopping_mall/shopping_mall/apps/oauth/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.response import Response
+
+from rest_framework.views import APIView
+
+from oauth.utils import OAuthQQ
+
+
+class QQAuthURLView(APIView):
+    """获取QQ的url"""
+    def get(self, request):
+        next = request.query_params.get("next")
+        oauth = OAuthQQ(state=next)
+        login_url = oauth.get_qq_login_url()
+
+        return Response({"login_url":login_url})
