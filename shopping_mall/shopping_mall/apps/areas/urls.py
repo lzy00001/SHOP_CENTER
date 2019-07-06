1dc4158cf671d54def3c7b921c9159fa760a9cb8
lzy00002 <812459596@qq.com>
2019-07-06 19:17:16 +0800

    新建地址中省市地区查询完成

---
+++ shopping_mall/shopping_mall/apps/areas/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+
+from areas import views
+
+
+router = DefaultRouter()
+router.register(r"areas", views.AreasViewSet,base_name="areas")
+
+urlpatterns = []
+
+urlpatterns += router.urls
--- shopping_mall/shopping_mall/apps/areas/views.py
+++ shopping_mall/shopping_mall/apps/areas/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from areas.models import Area
+from areas.serializers import AreaSerializer, SubAreaSerializer
+
+
+class AreasViewSet(ReadOnlyModelViewSet):
+    """行政区信息"""
+    pagination_class = None
+
+    def get_queryset(self):
+        """获取数据集"""
+        if self.action == "list":
+            return Area.objects.filter(parent=None)
+        else:
+            return Area.objects.all()
+
+    def get_serializer_class(self):
+        """获取序列化器"""
+        if self.action == "list":
+            return AreaSerializer
+        else:
+            return SubAreaSerializer
