b50ff9604fdc75e5a81a1c78745905dd4b0fcb36
lzy00002 <812459596@qq.com>
2019-07-07 19:29:50 +0800

    创建商品和广告的模型类

--- shopping_mall/shopping_mall/settings/dev.py
+++ shopping_mall/shopping_mall/settings/dev.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'verifications.apps.VerificationsConfig',
     'oauth.apps.OauthConfig',
     'areas.apps.AreasConfig',
+    'goods.apps.GoodsConfig',
+    'contents.apps.ContentsConfig',
 ]
 
 MIDDLEWARE = [
